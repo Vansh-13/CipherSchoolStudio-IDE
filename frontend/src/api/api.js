9a27c5e3514a61de213b4b3b12a222e6cec658de
Vansh Madaan <madaanvansh68@gmail.com>
2025-10-20 15:54:29 +0530

    done login/signup

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,10 +1,11 @@
-
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api";
+
+const BASE_URL = "https://cipherstudio-ide.onrender.com/api";
 
 export const api = axios.create({ baseURL: BASE_URL });
 
+// Optional: handle Authorization token
 export const setAuthToken = (token) => {
   if (token) {
     api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
